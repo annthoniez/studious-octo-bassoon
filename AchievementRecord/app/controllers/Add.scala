0ccf3f8c91eba56182fb9cca66861c9270f01f3d
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-04-10 22:58:14 +0700

    clean up

--- AchievementRecord/app/controllers/Add.scala
+++ AchievementRecord/app/controllers/Add.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import jp.t2v.lab.play2.auth.AuthElement
-import models.{Achievement, Auth, Student_Achievement}
+import models._
 import play.api.mvc.{AnyContent, Controller}
 import views.html
 
@@ -18,13 +18,12 @@ trait Add extends Controller with Pjax with AuthElement with AuthConfigImpl  {
     Ok(html.add_competition("competition"))
   }
 
-  def postCompettition = StackAction(AuthorityKey -> Seq(Auth.Student)) { implicit request =>
+  def postCompetition = StackAction(AuthorityKey -> Seq(Auth.Student)) { implicit request =>
     val body: AnyContent = request.body
     val textBody: Option[Map[String, Seq[String]]] = body.asFormUrlEncoded
-    val ach_id = Achievement.create(textBody.get("achievement_name")(0), textBody.get("date")(0), "test", textBody.get("reward")(0), textBody.get("category")(0), 1)
+    val ach_id = Achievement.create(textBody.get("achievement_name").head, textBody.get("date").head, "test", textBody.get("reward").head, textBody.get("category").head, 1)
     println(ach_id)
     Student_Achievement.create(loggedIn.username.value, ach_id)
-//    Achievement.insertToStudentAchievement(loggedIn.username.value, ach_id)
     Ok("Got" + textBody.toString)
 
   }
