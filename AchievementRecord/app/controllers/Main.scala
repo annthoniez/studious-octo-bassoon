9461c59a81091f28704c72f72e1cf351e3a26b89
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-04-21 18:20:58 +0700

    update ui

--- AchievementRecord/app/controllers/Main.scala
+++ AchievementRecord/app/controllers/Main.scala
@@ -10,6 +10,17 @@ import views.html
   */
 trait Main extends Controller with Pjax with AuthElement with AuthConfigImpl {
   def home = StackAction(AuthorityKey -> Seq(Auth.Student, Auth.Teacher, Auth.Staff)) { implicit request =>
+    val username = loggedIn.username.value
+    val th_name = Auth.valueOf(loggedIn.role_id) match {
+      case Auth.Student => models.Student.getProfile(username).th_name
+      case Auth.Staff => models.Staff.getProfile(username).th_name
+      case Auth.Teacher => models.Teacher.getProfile(username).th_name
+    }
+    val id = Auth.valueOf(loggedIn.role_id) match {
+      case Auth.Student => models.Student.getProfile(username).student_id
+      case Auth.Staff => models.Staff.getProfile(username).staff_id
+      case Auth.Teacher => models.Teacher.getProfile(username).teacher_id
+    }
     val achs = Auth.valueOf(loggedIn.role_id) match {
       case Auth.Student => models.Student.getAchs(loggedIn.username.value)
       case Auth.Teacher => models.Teacher.getAchs(loggedIn.username.value)
@@ -19,7 +30,7 @@ trait Main extends Controller with Pjax with AuthElement with AuthConfigImpl {
     val s_accs = Achievement.getStudentInAch(achs)
     val t_accs = Achievement.getTeacherInAch(achs)
     val orgs = Achievement.getOrgInAch(achs)
-    Ok(html.home("Home", achs2, s_accs, t_accs, orgs))
+    Ok(html.home("Home", achs2, s_accs, t_accs, orgs, th_name, id.value))
   }
 
   protected val main: User => Template = html.main.apply
