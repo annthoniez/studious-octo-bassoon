d198126ca0942a735ae87cd7c8dae9c601315713
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-04-22 04:34:59 +0700

    yer mak mak

--- AchievementRecord/app/controllers/Report.scala
+++ AchievementRecord/app/controllers/Report.scala
@@ -3,6 +3,7 @@ package controllers
 import java.io.{BufferedOutputStream, FileOutputStream}
 import java.time.LocalDateTime
 import java.util.UUID
+import scala.collection.JavaConversions._
 
 import it.innove.play.pdf.PdfGenerator
 import jp.t2v.lab.play2.auth.AuthElement
@@ -15,7 +16,7 @@ import views.html
   * Created by Pichai Sivawat on 4/20/2016.
   */
 trait Report extends Controller with Pjax with AuthElement with AuthConfigImpl {
-  val pdfGenerator: PdfGenerator = new PdfGenerator
+  var pdfGenerator: PdfGenerator = new PdfGenerator
 
   def index() = StackAction(AuthorityKey -> Seq(Auth.Staff)) { implicit request =>
     Ok(views.html.report("สร้างรายงาน - ระบบกรอกข้อมูลผลงานต่างๆ ของนักศึกษา"))
@@ -30,9 +31,16 @@ trait Report extends Controller with Pjax with AuthElement with AuthConfigImpl {
     result.foreach(println)
     val saveFileName = Crypto.sign(UUID.randomUUID().toString + LocalDateTime.now().toString + loggedIn.username.value)
     val bos = new BufferedOutputStream(new FileOutputStream(play.Play.application().path().toString + s"/public/pdfs/$saveFileName.pdf"))
-    Stream.continually(bos.write(pdfGenerator.toBytes(views.html.report("report"), "http://localhost:9000")))
+    val list = seqAsJavaList(Seq("fonts/THSarabunNew.ttf"))
+
+    pdfGenerator.loadLocalFonts(list)
+    Stream.continually(bos.write(pdfGenerator.toBytes(html.pdf(result), "http://localhost:9000")))
     bos.close()
     Ok(JsString(s"/assets/pdfs/$saveFileName.pdf"))
+  }
+
+  def pdf() = StackAction(AuthorityKey -> Seq(Auth.Staff)) { implicit request =>
+    Ok(html.pdf(null))
   }
 
   protected val main: User => Template = html.main.apply
