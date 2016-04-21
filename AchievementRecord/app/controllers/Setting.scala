edd5132f4e8bf2418295ff3bc8aa3463db5cd577
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-04-21 20:08:46 +0700

    setting page

---
+++ AchievementRecord/app/controllers/Setting.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import jp.t2v.lab.play2.auth.AuthElement
+import models._
+import play.api.mvc.Controller
+import views.html
+
+/**
+  * Created by Pichai Sivawat on 4/21/2016.
+  */
+trait Setting extends Controller with Pjax with AuthElement with AuthConfigImpl {
+  def index = StackAction(AuthorityKey -> Seq(Auth.Student, Auth.Staff, Auth.Teacher)) { implicit request =>
+    Ok(html.setting("Setting"))
+  }
+
+  protected val main: User => Template = html.main.apply
+}
+
+object Setting extends Setting
