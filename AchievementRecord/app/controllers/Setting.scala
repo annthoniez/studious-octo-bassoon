2a1b5349c3298a4c53a5f9e64f83fd01d4a0d206
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-04-22 22:32:51 +0700

    change password

--- AchievementRecord/app/controllers/Setting.scala
+++ AchievementRecord/app/controllers/Setting.scala
@@ -1,8 +1,9 @@
 package controllers
 
+import akka.actor.FSM.->
 import jp.t2v.lab.play2.auth.AuthElement
 import models._
-import play.api.mvc.Controller
+import play.api.mvc.{AnyContent, Controller}
 import views.html
 
 /**
@@ -11,6 +12,26 @@ import views.html
 trait Setting extends Controller with Pjax with AuthElement with AuthConfigImpl {
   def index = StackAction(AuthorityKey -> Seq(Auth.Student, Auth.Staff, Auth.Teacher)) { implicit request =>
     Ok(html.setting("ตั้งค่า - ระบบกรอกข้อมูลผลงานต่างๆ ของนักศึกษา"))
+  }
+
+  def changePassword = StackAction(AuthorityKey -> Seq(Auth.Student, Auth.Staff, Auth.Teacher)) { implicit request =>
+    val body: AnyContent = request.body
+    val textBody: Option[Map[String, Seq[String]]] = body.asFormUrlEncoded
+    println(textBody)
+
+    val password: String = textBody.get("password").head
+    val password_rep: String = textBody.get("password_rep").head
+
+    if (password == password_rep) {
+      Account.updateById(loggedIn.username).withAttributes(
+        'password -> password
+      )
+      Redirect(routes.Setting.index).flashing("result" -> "เปลี่ยนรหัสผ่านเรียบร้อย")
+    } else {
+      Redirect(routes.Main.tarwised)
+    }
+
+
   }
 
   protected val main: User => Template = html.main.apply
